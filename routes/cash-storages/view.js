ac329c1cdf4235ace8967cd6c780e5aca7fc119f
Zuarel <bubl174rus@gmail.com>
2019-03-31 21:59:25 +0500

    cash-storage-view next if not found

--- routes/cash-storages/view.js
+++ routes/cash-storages/view.js
@@ -11,6 +11,10 @@ module.exports = async (req, res, next) => {
         throw new Error('Id type error');
 
     const storages = await db.execQuery(`SELECT * FROM cash_storages WHERE id = ?`, [id]);
+    const [storage = {}] = storages;
+
+    if(!storage.id)
+        return next();
 
     res.render(__dirname + '/cash-storages-view', { storages });
 }
