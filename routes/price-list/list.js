9253d31e93316a7e9d01606a8381fa5f9b43a436
Zuarel <bubl174rus@gmail.com>
2019-04-05 00:01:55 +0500

    fix list output

--- routes/price-list/list.js
+++ routes/price-list/list.js
@@ -3,6 +3,7 @@ const db = require('../../libs/db');
 module.exports = async (req, res, next) => {
 
     const names = await db.execQuery(`SELECT name FROM cars GROUP BY name`);
+    const prices = await db.execQuery(`SELECT * FROM cars_price`);
 
-    return res.render(__dirname + '/price-list', { names });
+    return res.render(__dirname + '/price-list', { names, prices });
 }
