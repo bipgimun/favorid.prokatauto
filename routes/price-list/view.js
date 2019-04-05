c89964dc0dcbd22c80fa6c8dff09ed4e5574ef85
Zuarel <bubl174rus@gmail.com>
2019-04-05 23:36:58 +0500

    price-list view

--- routes/price-list/view.js
+++ routes/price-list/view.js
@@ -1,3 +1,10 @@
+const db = require('../../libs/db');
+
 module.exports = async (req, res, next) => {
-    res.render(__dirname + '/price-view');
+
+    const { id } = req.params;
+
+    const prices = await db.execQuery('SELECT * FROM cars_price WHERE id = ?', [id]);
+
+    res.render(__dirname + '/price-view', { prices });
 }
