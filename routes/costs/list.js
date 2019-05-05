92aa186d2fcb71b1d19daba6657c1a555b6975f1
Zuarel <bubl174rus@gmail.com>
2019-05-05 21:38:18 +0500

    now json

--- routes/costs/list.js
+++ routes/costs/list.js
@@ -12,6 +12,7 @@ module.exports = async (req, res, next) => {
 
     const carReservations = await db.execQuery(`SELECT *, CONCAT('CRR-', id) as code FROM cars_reservations`);
     const apartmentReservations = await db.execQuery(`SELECT *, CONCAT('APR-', id) as code FROM apartment_reservations`);
+    const cars = await db.execQuery(`SELECT *, CONCAT('CAR-', id) as code FROM cars WHERE company_property = 1`);
 
     const cashStorages = await db.execQuery(`SELECT * FROM cash_storages`);
     const costsCategories = await costsCategoriesModel.get();
@@ -20,9 +21,14 @@ module.exports = async (req, res, next) => {
         item.base = item.base_id || item.base_other;
     })
 
+    cars.forEach(car => {
+        car.subcode = `(${car.name} ${car.model} ${car.number})`;
+    })
+
     const groupDocuments = [
         { label: 'Аренда автомобилей', documents: carReservations, },
-        { label: 'Аренда квартир', documents: apartmentReservations, }
+        { label: 'Аренда квартир', documents: apartmentReservations, },
+        { label: 'Автомобили', documents: cars, },
     ];
 
     res.render(__dirname + '/costs-list', {
