08f33b677a748d9a4061b8a22b5b9a36bb7293ff
Zuarel <bubl174rus@gmail.com>
2019-06-02 22:35:16 +0500

    salary route

--- routes/salary-statement/index.js
+++ routes/salary-statement/index.js
@@ -1,6 +1,9 @@
+const router = require('express').Router();
 const db = require('../../libs/db');
 
-module.exports = async (req, res, next) => {
+router.get('/', async (req, res, next) => {
     const employees = await db.execQuery(`SELECT * FROM employees`);
     res.render(__dirname + '/salary-statement', { employees });
-}
+})
+
+module.exports = router;
