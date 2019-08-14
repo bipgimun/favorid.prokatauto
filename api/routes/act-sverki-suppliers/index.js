ccc8ea34ae1279f774538b5dd7f11a371943e112
zuarel <bubl174rus@gmail.com>
2019-08-15 04:18:49 +0500

    fix

--- api/routes/act-sverki-suppliers/index.js
+++ api/routes/act-sverki-suppliers/index.js
@@ -2,12 +2,6 @@ const router = require('express').Router();
 
 const Joi = require('joi');
 
-const acceptSchema = Joi.object({
-    period_left: Joi.date().iso().required(),
-    period_right: Joi.date().iso().required(),
-    customer_id: Joi.number().required(),
-});
-
 const db = require('../../../libs/db');
 const moment = require('moment');
 
@@ -62,110 +56,6 @@ router.post('/getTable', async (req, res, next) => {
         });
     });
 });
-
-router.get('/print', async function (req, res, next) {
-
-    const { period_left, period_right, customer_id, id } = await printSchema.validate(req.query);
-
-    const [customer] = await customersModel.get({ id: customer_id });
-
-    if (!customer)
-        throw new Error('Заказчик отсутствует');
-
-    let positions;
-    let money;
-    let saldoSum;
-    let saldoDate;
-
-    moment.locale('ru');
-
-    const [document = false] = id
-        ? (await db.execQuery(`SELECT * FROM act_sverki_documents WHERE id = ?`, [id]))
-        : [];
-
-    if (document.id) {
-        const details = await db.execQuery(`
-            SELECT * 
-            FROM act_sverki_documents_details 
-            WHERE document_id = ?`, [document.id]
-        );
-
-        positions = details.filter(item => item.code === 'CRR' || item.code === 'APR');
-
-        money = details.filter(item => item.income);
-
-        saldoSum = document.saldo;
-        saldoDate = moment(document.period_left).subtract(1, 'day').format('DD.MM.YYYY');
-    } else {
-        const calcResult = await calcTable({ period_left, period_right, customer_id });
-
-        money = calcResult.money;
-        positions = calcResult.positions;;
-
-        const saldoResult = await calcTable({
-            customer_id, period_right: moment(period_left).subtract(1, 'day').format('YYYY-MM-DD'),
-        });
-
-        saldoSum = saldoResult.totalSum;
-
-        saldoDate = moment(period_left).subtract(1, 'day').format('DD.MM.YYYY');
-    }
-
-    const currentDate = moment(new Date()).format('DD MMMM YYYY');
-
-    const dataArray = [...money, ...positions].map((item) => {
-        const { title, income, gone } = item;
-        return [title, income && income !== '--' ? income : '', gone && gone !== '--' ? gone : ''];
-    });
-
-    const file = await printAct({ customer, currentDate, saldoSum, dataArray, saldoDate, period_left: moment(period_left).format('DD.MM.YYYY'), period_right: moment(period_right).format('DD.MM.YYYY') });
-
-    res.download(path.join(process.cwd(), 'uploads', file), file, (error) => {
-        fs.unlinkSync(path.join(process.cwd(), 'uploads', file));
-    });
-})
-
-router.post('/document/save', async (req, res, next) => {
-    const { money: incomes, positions, customer_id, period_left, period_right, sum, saldo } = req.body;
-
-    if (!Array.isArray(incomes) || !Array.isArray(positions) || !customer_id) {
-        throw new Error('Ошибка входных переменных');
-    }
-
-    const document_id = await db.insertQuery(`INSERT INTO act_sverki_documents SET ?`, {
-        customer_id,
-        period_left,
-        period_right,
-        sum,
-        saldo,
-        manager_id: req.session.user.employee_id,
-    });
-
-    try {
-        for (const income of incomes) {
-            const { id: base_id, income: sum, title } = income;
-            await db.insertQuery(`INSERT INTO act_sverki_documents_details SET ?`, { document_id, base_id, income: sum, title });
-        }
-
-        for (const position of positions) {
-            const { id: base_id, gone, code, title } = position;
-            await db.insertQuery(`INSERT INTO act_sverki_documents_details SET ?`, { document_id, base_id, gone, code, title });
-        }
-    } catch (error) {
-        await db.execQuery(`DELETE FROM act_sverki_documents WHERE id = ?`, [document_id]);
-        throw new Error(error);
-    }
-
-    const [document] = await db.execQuery(`
-        SELECT d.*, c.name as customer_name 
-        FROM act_sverki_documents d 
-            LEFT JOIN customers c ON d.customer_id = c.id 
-        WHERE d.id = ?`, [document_id]);
-
-    document.created = moment(document.created).format('DD.MM.YYYY');
-
-    return res.json({ status: 'ok', data: document });
-})
 
 async function calcTable({ period_left = '', period_right = '', supplier_id = '', document = {} }) {
 
