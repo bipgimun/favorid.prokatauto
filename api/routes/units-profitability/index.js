cec5ea97b5f0f7f627d96d5a62eb3974d8589fea
zuarel <bubl174rus@gmail.com>
2019-12-09 18:22:19 +0500

    fix

--- api/routes/units-profitability/index.js
+++ api/routes/units-profitability/index.js
@@ -6,6 +6,7 @@ const moment = require('moment');
 const printFlowTable = require('../../../Service/printFlowTable');
 
 const db = require('../../../libs/db');
+
 const {
     costsCategories: costsCategoriesModel,
     drivers: driversModel,
@@ -71,13 +72,93 @@ async function calcData(time_min, time_max, unit) {
     const balanceAtStartPeriod = 0;
     const balanceAtEndPeriod = 0;
 
-    const [, typeOfUnit] = unit.match(/([\w\-]*)-\d*/);
+    const [, typeOfUnit, unitValue] = unit.match(/([\w\-]*)-(\d*)/);
 
-    if(!['APR, CAR, CUST'].includes(typeOfUnit)) {
+    if (!['APR', 'CAR', 'CUST', 'MUZ'].includes(typeOfUnit)) {
         throw new Error('Неверный тип объекта');
     }
 
-    
+    const calcFunction = getCalcFunction({ unit: typeOfUnit, unitValue, time_max, time_min });
+
+    calcFunction();
+
+    return {};
 }
+
+const getCalcFunction = ({ unit, unitValue, time_max, time_min }) => {
+
+    const dateGt = moment(time_min).set({ h: 0, m: 0 }).format('YYYY-MM-DD');
+    const dateLt = moment(time_max).set({ h: 23, m: 59 }).format('YYYY-MM-DD');
+    
+    const calcByUnit = {
+
+        'CAR': async function calcByCar() {
+
+            const costs = await db.execQuery(`
+                SELECT c.*,
+                    cd.price as sum
+                FROM costs c
+                    LEFT JOIN costs_details cd ON cd.cost_id = c.id
+                WHERE cd.target_type = ?
+                    AND cd.target_id = ?
+                    AND c.date BETWEEN '${dateGt}' AND '${dateLt}'
+                `, ['auto', unitValue]
+            );
+
+            // заявки с выбранным авто
+            const reservsByCar = await db.execQuery(`SELECT * FROM cars_reservations WHERE car_id = ?`, [unitValue]);
+            
+            // айди заявок, в которых присутствует авто
+            const reservsIds = reservsByCar.map(item => item.id).join(',');
+            
+            // выбор приходников по заявкам на аренду выбранного авто
+            const incomesByReserv = await db.execQuery(`
+                SELECT i.*
+                FROM incomes i
+                WHERE i.date BETWEEN '${dateGt}' AND '${dateLt}'
+                    AND i.code = 'CRR'
+                    AND i.document_id IN (?)
+                `, [reservsIds]
+            );
+            
+            const invoices = await db.execQuery(`
+                SELECT *
+                FROM invoices
+                WHERE code = 'CRR'
+                    AND base_id IN (?)
+                `, [reservsIds]
+            );
+
+
+            // выбор приходов по счетам по аренде авто
+            const incomesByInvoice = await db.execQuery(`
+                SELECT i.*
+                FROM incomes i
+                WHERE i.date BETWEEN '${dateGt}' AND '${dateLt}'
+                    AND i.code = 'pd'
+                    AND i.document_id in(?)
+                `, [invoicesId]
+            );
+            
+        },
+
+        'APR': ({ unitValue, time_max, time_min }) => {
+            console.log('apr');
+
+        },
+
+        'CUST': ({ unitValue, time_max, time_min }) => {
+            console.log('cust');
+
+        },
+        'MUZ': ({ unitValue, time_max, time_min }) => {
+            console.log('muz');
+
+        },
+    };
+
+    return calcByUnit[unit];
+}
+
 
 module.exports = app;
--- routes/units-profitability/index.js
+++ routes/units-profitability/index.js
@@ -5,10 +5,12 @@ module.exports = async (req, res, next) => {
     const cars = await db.execQuery(`SELECT * FROM cars`);
     const apartments = await db.execQuery(`SELECT * FROM apartments`);
     const customers = await db.execQuery(`SELECT * FROM customers`);
+    const contracts = await db.execQuery('SELECT * FROM muz_contracts');
 
     res.render(__dirname + '/units-profitability', {
         cars,
         apartments,
-        customers
+        customers,
+        contracts
     });
 }
